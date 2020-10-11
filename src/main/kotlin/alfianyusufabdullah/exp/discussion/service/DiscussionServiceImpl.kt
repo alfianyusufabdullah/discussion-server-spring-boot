72da0f175d1993feb3a2d9704fbe1ce5cd261c99
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-10-11 21:35:05 +0700

    implement new function to get all discussion

--- src/main/kotlin/alfianyusufabdullah/exp/discussion/service/DiscussionServiceImpl.kt
+++ src/main/kotlin/alfianyusufabdullah/exp/discussion/service/DiscussionServiceImpl.kt
@@ -2,15 +2,19 @@ package alfianyusufabdullah.exp.discussion.service
 
 import alfianyusufabdullah.exp.discussion.entity.DiscussionsEntity
 import alfianyusufabdullah.exp.discussion.model.NewDiscussionRequest
-import alfianyusufabdullah.exp.discussion.model.NewDiscussionResponse
+import alfianyusufabdullah.exp.discussion.model.DiscussionResponse
+import alfianyusufabdullah.exp.discussion.model.ListDiscussionRequest
+import alfianyusufabdullah.exp.discussion.model.ListDiscussionResponse
 import alfianyusufabdullah.exp.discussion.repository.DiscussionRepository
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.stream.Collectors
 
 @Service
 class DiscussionServiceImpl(val discussionRepository: DiscussionRepository) : DiscussionService {
 
-    override fun publishNewDiscussion(newDiscussionRequest: NewDiscussionRequest): NewDiscussionResponse {
+    override fun publishNewDiscussion(newDiscussionRequest: NewDiscussionRequest): DiscussionResponse {
         val newDiscussion = DiscussionsEntity(
                 idDiscussion = "${newDiscussionRequest.creatorUsername}-${System.currentTimeMillis()}",
                 creatorUsername = newDiscussionRequest.creatorUsername,
@@ -21,12 +25,31 @@ class DiscussionServiceImpl(val discussionRepository: DiscussionRepository) : Di
 
         discussionRepository.save(newDiscussion)
 
-        return NewDiscussionResponse(
+        return DiscussionResponse(
                 idDiscussion = newDiscussion.idDiscussion,
                 creatorUsername = newDiscussion.creatorUsername,
                 createdAt = newDiscussion.createdAt,
                 title = newDiscussion.title,
                 question = newDiscussion.question
+        )
+    }
+
+    override fun findAllDiscussions(listDiscussionRequest: ListDiscussionRequest): ListDiscussionResponse {
+        val pageDiscussions = discussionRepository.findAll(PageRequest.of(listDiscussionRequest.page, listDiscussionRequest.size))
+        val discussions = pageDiscussions.get().collect(Collectors.toList())
+
+        return ListDiscussionResponse(
+                pageDiscussions.totalPages,
+                pageDiscussions.size,
+                discussions.map {
+                    DiscussionResponse(
+                            idDiscussion = it.idDiscussion,
+                            creatorUsername = it.creatorUsername,
+                            createdAt = it.createdAt,
+                            title = it.title,
+                            question = it.question
+                    )
+                }
         )
     }
 }
