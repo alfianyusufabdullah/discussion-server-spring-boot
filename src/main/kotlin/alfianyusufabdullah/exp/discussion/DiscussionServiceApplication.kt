bfb2824f55ea1c1963d121cca3d690699000c6dc
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-10-11 17:56:01 +0700

    initial project :tada:

---
+++ src/main/kotlin/alfianyusufabdullah/exp/discussion/DiscussionServiceApplication.kt
@@ -0,0 +1,11 @@
+package alfianyusufabdullah.exp.discussion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscussionServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscussionServiceApplication>(*args)
+}
