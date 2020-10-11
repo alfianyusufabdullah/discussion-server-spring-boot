defb6c1deca8c354b36c0385445fb211bdd363fc
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-10-11 21:35:48 +0700

    added new rest for all discussion

--- src/main/kotlin/alfianyusufabdullah/exp/discussion/controller/DiscussionController.kt
+++ src/main/kotlin/alfianyusufabdullah/exp/discussion/controller/DiscussionController.kt
@@ -1,12 +1,8 @@
 package alfianyusufabdullah.exp.discussion.controller
 
-import alfianyusufabdullah.exp.discussion.model.DiscussionResponse
-import alfianyusufabdullah.exp.discussion.model.NewDiscussionRequest
-import alfianyusufabdullah.exp.discussion.model.NewDiscussionResponse
+import alfianyusufabdullah.exp.discussion.model.*
 import alfianyusufabdullah.exp.discussion.service.DiscussionService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class DiscussionController(val discussionService: DiscussionService) {
@@ -16,13 +12,34 @@ class DiscussionController(val discussionService: DiscussionService) {
             produces = ["application/json"],
             consumes = ["application/json"]
     )
-    fun publishNewDiscussion(@RequestBody body: NewDiscussionRequest): DiscussionResponse<NewDiscussionResponse> {
+    fun publishNewDiscussion(@RequestBody body: NewDiscussionRequest): WebResponse<DiscussionResponse> {
         val newDiscussionResponse = discussionService.publishNewDiscussion(body)
 
-        return DiscussionResponse(
+        return WebResponse(
                 code = 200,
                 message = "Successfully added new discussion",
                 data = newDiscussionResponse
+        )
+    }
+
+    @GetMapping(
+            value = ["/api/discussion"],
+            produces = ["application/json"]
+    )
+    fun findAllDiscussions(
+            @RequestParam("size", defaultValue = "10") size: Int,
+            @RequestParam("page", defaultValue = "0") page: Int): WebResponse<ListDiscussionResponse> {
+        val request = ListDiscussionRequest(page, size)
+        val response = discussionService.findAllDiscussions(request)
+
+        return WebResponse(
+                code = 200,
+                message = "Successfully retrieve discussions",
+                data = ListDiscussionResponse(
+                        response.page,
+                        response.size,
+                        response.discussions
+                )
         )
     }
 }
