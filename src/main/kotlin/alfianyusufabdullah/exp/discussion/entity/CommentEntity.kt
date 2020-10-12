83ad297844a66666acccb245255db7a7e8d17b0c
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-10-12 21:12:52 +0700

    added comment entity

---
+++ src/main/kotlin/alfianyusufabdullah/exp/discussion/entity/CommentEntity.kt
@@ -0,0 +1,27 @@
+package alfianyusufabdullah.exp.discussion.entity
+
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class CommentEntity(
+        @Id
+        val idComment: String,
+
+        @Column(name = "id_parent")
+        val idParent: String,
+
+        @Column(name = "creator_username")
+        val creatorUsername: String,
+
+        @Column(name = "creator_at")
+        val createdAt: Date,
+
+        @Column(name = "title")
+        val title: String,
+
+        @Column(name = "creator_username")
+        val comment: String
+)
